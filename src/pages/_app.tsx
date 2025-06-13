7a64b44629f0c3a474d11e93b37c6f116e3364b8
BluenSaint <okoyankee@gmail.com>
2025-06-13 14:40:50 -0400

    update

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '@/styles/globals.css'
-import { startScheduler } from '@/jobs/scheduler'
-
-// Start the sentiment check scheduler
-startScheduler();
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-} 
